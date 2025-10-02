de9c87a69f1f3f31955a6779a968f024a2c59ca0
Appolo-15 <rayl1x.gta@gmail.com>
2025-10-02 17:35:37 +0200

    Add SoundManager with volume control and mute support using NAudio

--- Lesson 10.1/HangmanGame/Core/SoundManager.cs
+++ Lesson 10.1/HangmanGame/Core/SoundManager.cs
@@ -1,65 +1,68 @@
 using System;
-using System.Media;
+using Data;
+using NAudio.Wave;
 
 namespace HangmanGame.Core
 {
-    public class SoundManager
+    public class SoundManager : IDisposable
     {
+        private readonly Settings settings;
+        private WaveOutEvent? outputDevice;
+        private AudioFileReader? audioFile;
+
+        public SoundManager(Settings settings)
+        {
+            this.settings = settings;
+        }
+
         public void PlaySound(string soundFileName)
         {
-            SoundPlayer player = new SoundPlayer($"Sounds/{soundFileName}.wav");
-            player.Play();
+            if (!settings.SoundEnabled)
+            {
+                return;
+            }
+
+            DisposeCurrentAudio();
+
+            try
+            {
+                audioFile = new AudioFileReader($"Assets/Sounds/{soundFileName}.wav")
+                {
+                    Volume = settings.Volume / 10f // for example  0-10 will be 0.0-0.10
+                };
+
+                outputDevice = new WaveOutEvent();
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine($"Error playing sound \"{soundFileName}\": {e.Message}");
+            }
         }
 
-        public void PlayMenuOpen()
-        {
-            PlaySound("Menu/menu_open");
-        }
-
-        public void PlayMenuSelect()
-        {
-            PlaySound("Menu/menu_select");
-        }
-
-        public void PlayMenuInvalid()
-        {
-            PlaySound("Menu/menu_invalid");
-        }
-
-        public void PlayCorrectLetter()
-        {
-            PlaySound("Game/correct_letter");
-        }
-
+        public void PlayMenuOpen() => PlaySound("Menu/menu_open");
+        public void PlayMenuSelect() => PlaySound("Menu/menu_select");
+        public void PlayMenuInvalid() => PlaySound("Menu/menu_invalid");
+        public void PlayCorrectLetter() => PlaySound("Game/correct_letter");
         public void PlayWrongLetter()
         {
             PlaySound("Game/wrong_letter");
             PlaySound("Game/writing");
         }
-
-        public void PlayHint()
+        public void PlayHint() => PlaySound("Game/hint");
+        public void PlayWordCompleted() => PlaySound("Game/word_completed");
+        public void PlayLoose() => PlaySound("Game/loose");
+        public void PlayKeyPress() => PlaySound("Interface/keyboard_click");
+        public void PlayTick() => PlaySound("Timer/tick");
+        private void DisposeCurrentAudio()
         {
-            PlaySound("Game/hint");
+            outputDevice?.Stop();
+            outputDevice?.Dispose();
+            audioFile?.Dispose();
+            outputDevice = null;
+            audioFile = null;
         }
-
-        public void PlayWordCompleted()
-        {
-            PlaySound("Game/word_completed");
-        }
-
-        public void PlayLoose()
-        {
-            PlaySound("Game/loose");
-        }
-
-        public void PlayKeyPress()
-        {
-            PlaySound("Interface/keyboard_click");
-        }
-
-        public void PlayTick()
-        {
-            PlaySound("Timer/tick");
-        }
+        public void Dispose() => DisposeCurrentAudio();
     }
 }
