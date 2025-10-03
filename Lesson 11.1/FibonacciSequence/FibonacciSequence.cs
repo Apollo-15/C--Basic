4e7a1b071b189ebd2c2224f66e1d77178231e3bd
Appolo-15 <rayl1x.gta@gmail.com>
2025-10-03 17:55:44 +0200

    FibonacciSequence | Fixed

--- Lesson 11.1/FibonacciSequence/FibonacciSequence.cs
+++ Lesson 11.1/FibonacciSequence/FibonacciSequence.cs
@@ -6,7 +6,7 @@ namespace FibonacciSequence
     {
         public static int GetFibonacci(int n)
         {
-            if (n == 1) return 0;
+            if (n <= 0) return 0;
             if (n == 2) return 1;
             return GetFibonacci(n - 1) + GetFibonacci(n - 2);
         }
