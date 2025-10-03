6c6483012249d55e52b0d38d49efe4c81885cba7
Appolo-15 <rayl1x.gta@gmail.com>
2025-10-03 17:53:33 +0200

    FibonacciSequence | Done

---
+++ Lesson 11.1/FibonacciSequence/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FibonacciSequence
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            FibonacciSequence.Run();
+        }
+    }
+}
